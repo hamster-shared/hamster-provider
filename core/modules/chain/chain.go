c491fe213a44d917dbfe85e618dc382ea518b080
mohaijiang <haijiang.mo@newtouch.com>
2022-02-14 14:31:30 +0800

    Optimize the structure

--- core/modules/chain/chain.go
+++ core/modules/chain/chain.go
@@ -520,6 +520,7 @@ func (cc *ChainClient) GetResource(resourceIndex uint64) (*ComputingResource, er
 		return nil, err
 	}
 
+	//bytes, err := types.EncodeToBytes([]types.U64{types.NewU64(resourceIndex)})
 	bytes, err := types.EncodeToBytes(types.NewU64(resourceIndex))
 	if err != nil {
 		return nil, err
@@ -532,9 +533,16 @@ func (cc *ChainClient) GetResource(resourceIndex uint64) (*ComputingResource, er
 	}
 	fmt.Println(key.Hex())
 
+	rows, err := cc.api.RPC.State.GetStorageRawLatest(key)
+	fmt.Println("rows", len(*rows))
+	fmt.Println("err:", err)
+	fmt.Println("row:", rows)
+
 	var computingResource ComputingResource
+
 	ok, err := cc.api.RPC.State.GetStorageLatest(key, &computingResource)
 	if !ok {
+		fmt.Println(err)
 		return nil, errors.New("cannot get state with computingResource")
 	}
 
--- frontend/mock/provider/boot.ts
+++ frontend/mock/provider/boot.ts
@@ -1,21 +1,21 @@
-import {resultSuccess} from "../_util";
+// import {resultSuccess} from "../_util";
 import {MockMethod} from "vite-plugin-mock";
 
 export default [
-  {
-    url: '/api/v1/config/boot',
-    timeout: 1000,
-    method: 'post',
-    response: () => {
-      return resultSuccess('')
-    },
-  },
-  {
-    url: '/api/v1/config/boot',
-    timeout: 1000,
-    method: 'get',
-    response: () => {
-      return resultSuccess(false)
-    },
-  },
+  // {
+  //   url: '/api/v1/config/boot',
+  //   timeout: 1000,
+  //   method: 'post',
+  //   response: () => {
+  //     return resultSuccess('')
+  //   },
+  // },
+  // {
+  //   url: '/api/v1/config/boot',
+  //   timeout: 1000,
+  //   method: 'get',
+  //   response: () => {
+  //     return resultSuccess(false)
+  //   },
+  // },
 ] as MockMethod[];
